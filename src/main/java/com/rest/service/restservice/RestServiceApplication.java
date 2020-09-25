c198ee71aa72195827ad14dfc3d4172c0079e197
apfdamascena <apfd@cin.ufpe.br>
2020-09-25 16:14:45 -0300

    adding admin server

--- src/main/java/com/rest/service/restservice/RestServiceApplication.java
+++ src/main/java/com/rest/service/restservice/RestServiceApplication.java
@@ -1,9 +1,10 @@
 package com.rest.service.restservice;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+@EnableAdminServer
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
