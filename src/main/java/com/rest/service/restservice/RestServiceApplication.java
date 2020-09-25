13d14187e8382064b5aae1c54d2bae9a6ac258fd
apfdamascena <apfd@cin.ufpe.br>
2020-09-25 16:27:11 -0300

    adding others dependencies

--- src/main/java/com/rest/service/restservice/RestServiceApplication.java
+++ src/main/java/com/rest/service/restservice/RestServiceApplication.java
@@ -3,8 +3,11 @@ package com.rest.service.restservice;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 @EnableAdminServer
+@SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
