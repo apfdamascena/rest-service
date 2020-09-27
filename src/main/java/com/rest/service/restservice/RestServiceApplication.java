001120b36868b6f842897fe19a8e8185ffaa64e7
apfdamascena <apfd@cin.ufpe.br>
2020-09-27 15:31:20 -0300

    http settings

--- src/main/java/com/rest/service/restservice/RestServiceApplication.java
+++ src/main/java/com/rest/service/restservice/RestServiceApplication.java
@@ -4,8 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 @EnableAdminServer
 @SpringBootApplication
 public class RestServiceApplication {
@@ -13,4 +18,20 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
+
+	@Configuration
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+			http.logout().logoutUrl("/logout");
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
+			http.authorizeRequests().antMatchers("/**").authenticated();
+
+			http.httpBasic();
+		}
+	}
+
 }
