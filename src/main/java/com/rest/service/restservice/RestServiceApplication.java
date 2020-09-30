8ab3552ccd8cc439300d6db7f9f089d9fa15fa2d
apfdamascena <apfd@cin.ufpe.br>
2020-09-29 23:33:52 -0300

    remove admin client server

--- src/main/java/com/rest/service/restservice/RestServiceApplication.java
+++ src/main/java/com/rest/service/restservice/RestServiceApplication.java
@@ -1,35 +1,14 @@
 package com.rest.service.restservice;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-@EnableAdminServer
+
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
-	}
-
-	@Configuration
-	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-			http.logout().logoutUrl("/logout");
-			http.csrf().disable();
-
-			http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
-			http.authorizeRequests().antMatchers("/**").authenticated();
-
-			http.httpBasic();
-		}
 	}
 }
